fb66bec1e3d3e1ca90ecf8cc532b105e3fc62d13
Nick Gagliardi <ngagliar@msudenver.edu>
2020-02-29 22:01:15 +0000

    Fixed bugs with plotting and added comment details

--- main.py
+++ main.py
@@ -1,76 +1,71 @@
-
-# Select a dataset with binary target values.
-from sklearn.datasets import load_diabetes
-
-
-# Use pandas to read CSV file as dataframe.
-import pandas as panda
-
-col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
-pima = panda.read_csv("pima-indians-diabetes.csv", header=None, names=col_names)
-print(pima)
-
-# select 5 features 
-feature_cols = ['pregnant', 'skin', 'bmi', "pedigree", 'label']
-X = pima[feature_cols]
-print(X)
-
-# Use "train_test_split" from "sklearn.cross_validationtrain" to split test/training data
+"""
+    References:
+        - sciKit learn website documentation on Logistic Regression
+        - Class slides
+        - Class provide demos
+        - Office Hours with Professor
+    Description:
+        Logistic Regression for Binary Classification using Pima Indians Diabetes Dataset
+"""
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
 from sklearn.model_selection import train_test_split
 
-pimaSK = load_diabetes()
-x_train, x_test, y_train, y_test = train_test_split(pimaSK.data, pimaSK.target, test_size=0.4, random_state=0)
+# 1. & 2. Select and Load Pima Indians Diabetes Data into a pandas Dataframe
+col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'Outcome']
+pima = pd.read_csv('pima-indians-diabetes.csv', header=None, names=col_names)
 
+# 3. Select 5 features and true outcome
+feature_cols = ['pregnant', 'skin', 'bmi', 'age', 'glucose']
+X = pima[feature_cols]
+y = pima['Outcome']
 
-# Fit your model with training data and test your model after fitting.
-from sklearn.linear_model import LogisticRegression 
-logisticRegr = LogisticRegression()
+# 4. Split into test and training data
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=0)
 
-logisticRegr.fit(x_train, y_train)
-logisticRegr.predict(x_test[0].reshape(1, -1))
-logisticRegr.predict(x_test[0:10])
-predictions = logisticRegr.predict(x_test)
+# 5. fit the model with data and test
+logreg = LogisticRegression()
+logreg.fit(x_train, y_train)
+y_pred = logreg.predict(x_test)
 
+# 6. Confusion Matrix
+cnf_matrix = metrics.confusion_matrix(y_test, y_pred)
+print('Confusion Matrix: ', cnf_matrix)
 
-# Use score method to get accuracy of model
-score = logisticRegr.score(x_test, y_test)
-print(score)
-
-# Calculate and Plot out confusion matrix
-import matplotlib.pyplot as plt 
-import seaborn as sns
-from sklearn import metrics
-
-cm = metrics.confusion_matrix(y_test, predictions)
-print(cm)
-
-
-# Seaborn
-plt.figure(figsize=(9,9))
-sns.heatmap(cm, annot=True, fmt=".3f", linewidths=.5, square = True, cmap = 'Blues_r');
-plt.ylabel('Actual label');
-plt.xlabel('Predicted label');
-all_sample_title = 'Accuracy Score: {0}'.format(score)
-plt.title(all_sample_title, size = 15);
-
-# Matplotlib
-import numpy as np
-plt.figure(figsize=(9,9))
-plt.imshow(cm, interpolation='nearest', cmap='Pastel1')
-plt.title('Confusion matrix', size = 15)
-plt.colorbar()
-tick_marks = np.arange(10)
-plt.xticks(tick_marks, ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], rotation=45, size = 10)
-plt.yticks(tick_marks, ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], size = 10)
+class_names = [0, 1]
+fig, ax = plt.subplots()
+tick_marks = np.arange(len(class_names))
+plt.xticks(tick_marks, class_names)
+plt.yticks(tick_marks, class_names)
+sns.heatmap(pd.DataFrame(cnf_matrix), annot=True, cmap="YlGnBu", fmt='g')
+sns.heatmap(cnf_matrix, annot=True, cmap="YlGnBu", fmt='g')
+ax.xaxis.set_label_position("top")
 plt.tight_layout()
-plt.ylabel('Actual label', size = 15)
-plt.xlabel('Predicted label', size = 15)
-width, height = cm.shapefor 
+plt.title('Confusion matrix', y=1.1)
+plt.ylabel('Actual Outcome')
+plt.xlabel('Predicted Outcome')
+plt.figure()    # plot Confusion Matrix
 
-for x in range(width):
- for y in range(height):
-  plt.annotate(str(cm[x][y]), xy=(y, x), 
-  horizontalalignment='center',
-  verticalalignment='center')
 
-  
+# 6. Precision Score, Recall Score, F Score
+print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+print("Precision:", metrics.precision_score(y_test, y_pred))
+print("Recall:", metrics.recall_score(y_test, y_pred))
+print(metrics.classification_report(y_test, y_pred))
+
+# 7. Plot ROC curve and print out the ROC_AUC Score
+y_pred_proba = logreg.predict_proba(x_test)[::, 1]
+fpr, tpr, thresholds = metrics.roc_curve(y_test, y_pred_proba)
+auc = metrics.roc_auc_score(y_test, y_pred_proba)
+print(auc)      # print out AUC score
+plt.plot(fpr, tpr, label="Logistic Regression, auc=" + str(auc))
+plt.plot([0, 1], [0, 1], 'r--')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Logistic Regression for Binary Classification')
+plt.legend(loc=4)
+plt.show()    # plot ROC Curve
